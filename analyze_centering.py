22c53822ad54532c4b8c0eded576d819265541ca
bashkim <bashkim.mehmeti@broadridge.com>
2023-10-17 11:29:34 +0200

    somewhat working centering

--- analyze_centering.py
+++ analyze_centering.py
@@ -1,39 +1,79 @@
 import cv2
+import numpy as np
+import os
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator  # ultralytics.yolo.utils.plotting is deprecated
 
-class analyze_centering:
-    def __init__(self, bild_pfad):
-        self.bild_pfad = bild_pfad
+model = YOLO('runs/detect/train19/weights/best.pt')
+
+def analyze_centering(bild_pfad):
+
+    # Lade das Bild
+    display_bild = cv2.imread(bild_pfad)
+   
+    # rotate picture
+    # https://stackoverflow.com/questions/57713358/how-to-rotate-skewed-fingerprint-image-to-vertical-upright-position
+    gray = cv2.cvtColor(display_bild, cv2.COLOR_BGR2GRAY)
+    gray = 255 - gray + 60
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    # Compute rotated bounding box
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1] 
+
+    print("PRE:" + str(angle))
+
+    if angle < 45:
+        angle = -angle
+    else:
+        angle = 90 - angle
+
+    print("POST:" + str(angle))
+
+    # Rotate image to deskew
+    (h, w) = display_bild.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(display_bild, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+    # Run YOLOv8 tracking on the frame, persisting tracks between frames
+    results = model.track(rotated)
+
+    # Visualize the results on the frame
+    annotated_frame = results[0].plot()
+
+    #move windows
+    #https://stackoverflow.com/questions/62450255/can-you-display-an-image-at-specific-screen-coordinates-with-opencv
+    # Make your windows
+    cv2.namedWindow('Raw Image', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Inverted Image', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Rotated Image', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('YOLOv8 Tracking', cv2.WINDOW_NORMAL)
+
+    if display_bild is None:
+        print(f"Fehler: Bild {bild_pfad} konnte nicht geladen werden.")
+    else:
+        # Zeigen Sie das Bild an (z.B., in einem separaten Fenster)
+        cv2.imshow('Raw Image', display_bild)
+        cv2.imshow('Inverted Image', thresh)
+        cv2.imshow('Rotated Image', rotated)
+        cv2.imshow('YOLOv8 Tracking', annotated_frame)
+
+        # Then move your windows to where you want them
+        cv2.moveWindow('Raw Image', 100, 50)
+        cv2.moveWindow('Inverted Image', 1400, 50)
+        cv2.moveWindow('Rotated Image', 2700, 50)
+        cv2.moveWindow('YOLOv8 Tracking', 4000, 50)
+
+        cv2.waitKey(0)  # Warten auf eine Taste, bevor das Fenster geschlossen wird
+        cv2.destroyAllWindows()  # Schließen Sie alle offenen OpenCV-Fenster
     
-    def analyze(self):
-        # Lade das Bild
-        bild = cv2.imread(self.bild_pfad)
-        
-        if bild is None:
-            print("Fehler: Bild konnte nicht geladen werden.")
-            return None
-        
-        # Führen Sie hier Ihre Zentrierungsanalyse durch
-        # Zum Beispiel, ermitteln Sie das Zentrum des Bildes oder prüfen Sie, ob das Bild zentriert ist
-        
-        # Beispiel: Berechnen Sie die Mitte des Bildes
-        höhe, breite, _ = bild.shape
-        mitte_x = breite // 2
-        mitte_y = höhe // 2
-        
-        # Beispiel: Überprüfen Sie, ob die Mitte des Bildes nahe am tatsächlichen Zentrum ist
-        toleranz = 10
-        ist_zentriert = abs(mitte_x - breite/2) <= toleranz and abs(mitte_y - höhe/2) <= toleranz
-        
-        # Hier können Sie weitere Analysen und Berechnungen durchführen
-        
-        # Geben Sie das Analyseergebnis als Tupel zurück (ohne "ist_zentriert")
-        analyse_ergebnis = (mitte_x, mitte_y)
-        
-        return analyse_ergebnis
+    analyse_ergebnis = 10
+
+    return analyse_ergebnis
 
 if __name__ == "__main__":
-    # Beispielverwendung der Klasse
+    # Beispielverwendung der Funktion
     bild_pfad = "pfad_zum_bild.jpg"
-    analyzer = analyze_centering(bild_pfad)
-    mitte_x, mitte_y = analyzer.analyze()
+    mitte_x, mitte_y = analyze_centering(bild_pfad)
     print("Zentrum des Bildes (X, Y):", mitte_x, mitte_y)
--- data_reader.py
+++ data_reader.py
@@ -3,22 +3,23 @@ import time
 import shutil
 import zipfile
 import json
+
 # Importieren Sie die Funktionen oder Klassen aus den Analyse-Skripten
-from analyze_centering import analyse_centering
+from analyze_centering import analyze_centering
 from analyze_edge import analyse_edge
 from analyze_corner import analyse_corner
 
 # Das Verzeichnis, das Sie überwachen möchten
-verzeichnis_pfad = '/pfad/zum/verzeichnis'
+verzeichnis_pfad = 'sourceDirectory'
 
 # Das Verzeichnis, in das die Auswertungsdateien verschoben werden sollen
-ziel_verzeichnis = '/pfad/zum/zielverzeichnis'
+ziel_verzeichnis = 'destinationDirectory'
 
 # Das Verzeichnis, in dem die Auswertungen gespeichert werden sollen
-auswertungs_verzeichnis = '/pfad/zum/auswertungsverzeichnis'
+auswertungs_verzeichnis = 'gradedCardsDirectory'
 
 # Entpacken Sie die Zip-Datei in ein temporäres Verzeichnis
-temp_verzeichnis = '/pfad/zum/temporären/verzeichnis'
+temp_verzeichnis = 'tempDirectory'
 
 # Initialisiere eine Liste der bereits verarbeiteten Dateien
 verarbeitete_dateien = set()
@@ -30,14 +31,14 @@ def analyse_json(json_pfad):
         # Hier können Sie mit den JSON-Daten arbeiten
         return json_inhalt
 
-def verarbeite_bild(json_array, bild_pfad):
+def verarbeite_bild(bild_pfad):
     # Hier können Sie die Verarbeitung des Bildes unter Verwendung des JSON-Arrays durchführen
     bearbeitetes_array = []
     
     # Rufen Sie die entsprechenden Funktionen oder Klassen aus den Analyse-Skripten auf
-    bearbeitetes_array_centering = analyse_centering(json_array, bild_pfad)
-    bearbeitetes_array_edge = analyse_edge(json_array, bild_pfad)
-    bearbeitetes_array_corner = analyse_corner(json_array, bild_pfad)
+    bearbeitetes_array_centering = analyze_centering(bild_pfad)
+    bearbeitetes_array_edge = analyse_edge(bild_pfad)
+    bearbeitetes_array_corner = analyse_corner(bild_pfad)
     
     # Fügen Sie die Ergebnisse der Analysen dem bearbeiteten Array hinzu oder verarbeiten Sie sie
     # bearbeitetes_array.extend(bearbeitetes_array_centering)
@@ -64,6 +65,7 @@ while True:
             # Überprüfen, ob es sich um eine Zip-Datei handelt
             if datei.endswith('.zip'):
                 with zipfile.ZipFile(quelle, 'r') as zip_datei:
+                    print(f'Inhalt:{datei} verschoben nach {temp_verzeichnis}')
                     zip_datei.extractall(temp_verzeichnis)
                 
                 # Hier können Sie die JSON-Datei verarbeiten und das JSON-Array erhalten
@@ -82,7 +84,7 @@ while True:
                     temp_pfad = os.path.join(temp_verzeichnis, temp_datei)
                     if temp_datei.endswith('.jpg') or temp_datei.endswith('.png'):
                         # Verarbeiten Sie das Bild unter Verwendung des JSON-Arrays
-                        bearbeitetes_array = verarbeite_bild(json_array, temp_pfad)
+                        bearbeitetes_array = verarbeite_bild(temp_pfad)
                 
                 # Speichern Sie das bearbeitete Array in einer separaten Textdatei im Auswertungsverzeichnis
                 auswertungs_datei = os.path.splitext(datei)[0] + '_auswertung.txt'
@@ -92,14 +94,20 @@ while True:
                     auswertungsdatei.write(json.dumps(bearbeitetes_array))  # Schreiben Sie das Array als JSON in die Datei
                 
                 # Verschieben Sie die verarbeiteten Dateien in das Zielverzeichnis
-                ziel = os.path.join(ziel_verzeichnis, datei)
-                shutil.move(quelle, ziel)
-                print(f'Datei verschoben nach {ziel}')
+                #ziel = os.path.join(ziel_verzeichnis, datei)
+                #shutil.move(quelle, ziel)
+                #print(f'Datei verschoben nach {ziel}')
                 
-                # Löschen Sie das temporäre Verzeichnis
-                shutil.rmtree(temp_verzeichnis)
+            # Löschen Sie das temporäre Verzeichnis
+            for temp_datei in temp_dateien:
+                if temp_datei.endswith('.jpg') or temp_datei.endswith('.png'):
+                    print(temp_verzeichnis + temp_datei )
+                    try:
+                        os.remove(temp_verzeichnis+ "/" +temp_datei)
+                    except:
+                        print("FileNotFoundError:Errno 2] No such file or directory: '" + temp_datei + "")
         
-        # Fügen Sie die neu verarbeiteten Dateien zur Liste der verarbeiteten Dateien hinzu
+        # Fügen Sie die neu verarbeiteten Dateien zur Liste der verarbeiteten Dateien hinzu 
         verarbeitete_dateien.update(neue_dateien)
     
     # Warten Sie eine Weile, bevor Sie erneut nach neuen Dateien suchen
